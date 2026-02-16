2cdd53e962d7fbf2f5483a9a403599c33f88ee26
Azadova Indira <aindira2197@gmail.com>
2026-02-16 19:16:50 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+# 1
+n = int(input())
+for i in range(n):
+    print(i)
+
+# 2
+n = int(input())
+for i in range(n):
+    print(n - i)
+
+# 3
+n = int(input())
+print(n % 2 == 0)
+
+# 4
+n = int(input())
+print(n % 3 == 0)
+
+# 5
+n = int(input())
+print(n % 5 == 0)
